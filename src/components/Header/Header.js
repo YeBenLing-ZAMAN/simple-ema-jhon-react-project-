f1cb4be57443d058f1941ec474917f0a981e5d71
MD KAMRUZZAMAN <kamruzzamanmd@hotmail.com>
2022-03-22 10:42:25 +0600

    header navbar is completed

---
+++ src/components/Header/Header.js
@@ -0,0 +1,20 @@
+import './Header.css'
+import React from 'react';
+import logo from '../../images/Logo.svg'
+
+const Header = () => {
+    return (
+        <nav className='container'>
+            <div className='left-container'>
+                <img src={logo} alt="" />
+            </div>
+            <div className='right-container'>
+                <li>Order</li>
+                <li>Order Review</li>
+                <li>Manage Inventory</li>
+            </div>
+        </nav>
+    );
+};
+
+export default Header;
